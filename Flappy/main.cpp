3bf78b52cb4c51e474c0096e936e0033a20894fb
VarunIrani <varunirani0505@gmail.com>
2020-03-13 15:12:17 +0530

    Initial Commit

---
+++ Flappy/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Flappy
+//
+//  Created by Varun Irani on 13/03/20.
+//  Copyright © 2020 Varun Irani. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
